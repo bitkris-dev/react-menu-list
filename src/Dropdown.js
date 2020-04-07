f841040d2554304a464bbe7fc1103d29e419be6b
Kristian Barrese <kristian.webdeveloper@gmail.com>
2020-04-07 18:23:07 +0200

    Allow dropdown custom className and style

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,26 +1,38 @@
 /* @flow */
 
-import React from 'react';
-import type {Node as ReactNode} from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import type { Node as ReactNode } from "react";
+import PropTypes from "prop-types";
 
 type Props = {
-  children?: ReactNode;
+  children?: ReactNode,
+  className?: string,
+  style?: Object,
 };
 
 export default class Dropdown extends React.Component<Props> {
   static propTypes = {
-    children: PropTypes.node
+    children: PropTypes.node,
+    className: PropTypes.string,
+    style: PropTypes.object,
   };
 
   render() {
+    var style = _objectSpread(
+      {},
+      {
+        background: "white",
+        border: "1px solid rgba(0,0,0,.2)",
+        boxShadow: "0 2px 4px rgba(0,0,0,0.2)",
+        padding: "6px 0",
+        style: style,
+      },
+      {},
+      this.props.style || {}
+    );
+
     return (
-      <div style={{
-        background: 'white',
-        border: '1px solid rgba(0,0,0,.2)',
-        boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
-        padding: '6px 0'
-      }}>
+      <div className={this.props.className} style={style}>
         {this.props.children}
       </div>
     );
